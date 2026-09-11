8332365762b29daa74ea5e809105b57bc0642e89
Heeseo <yhs040421@ewha.ac.kr>
2026-09-12 01:17:58 +0900

    Add triangles to form pentagon

--- Assets/S03/S03_CustomPolygonMesh_Pentagon.cs
+++ Assets/S03/S03_CustomPolygonMesh_Pentagon.cs
@@ -18,7 +18,9 @@
           // TODO 2: 정점 3개씩 묶어 삼각형들을 구성하세요
           int[] triangles = new int[]
           {
-
+            0, 1, 2,
+            0, 2, 3,
+            0, 3, 4,
           };
 
           Mesh mesh = new Mesh();
